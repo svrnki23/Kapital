42feaa6a8ed6cb3e9243ca842df2b294fa36f422
svrnki23 <srikar1330@gmail.com>
2025-12-04 19:38:59 -0600

    added footer content, styling pending

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Book from './components/book';
+import Footer from './components/Footer';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       {/* /*placeholder to just visualize things, booking section below video isn't implemented yet */}
       <Book /> 
+      <Footer />
     </div>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,56 @@ import React from "react";
 import './footer.css';
 
 const Footer = () => {
-    return {
-        
+    return (
+        <footer className="footer-container">
+            <div className="footer-content">
 
-    }
+                {/* Column 1 - brand and vision */}
+                <div className="footer-brand">
+                    <h2 className="shiny-gold">KAPITAL</h2>
+                    <p>Experience the thrill of the drive. Premium luxury vehicles available at your command.</p>
+                </div>
+
+
+                {/* Column 2 - explore links */}
+                <div className="footer-column">
+                    <h4 className="shiny-gold">EXPLORE</h4>
+                    <a href="#fleet">THE GARAGE</a>
+                    <a href="#services">Services</a>
+                    <a href="#membership">Membership</a>
+                    <a href="#about">About</a>
+                </div>
+
+
+                {/* Column 3 - legal support  */}
+                <div className="footer-column">
+                    <h4 className="shiny-gold">SUPPORT</h4>
+                    <a href="#help">Help Center</a>
+                    <a href="#terms">Terms & Conditions</a>
+                    <a href="#privacy">Privacy Policy</a>
+                    <a href="#contact">Contact</a>
+                </div>
+
+                {/* Column 4 - Contact info */}
+                <div className="footer-column">
+                    <h4 className="shiny-gold">VISIT US</h4>
+                    <p>123 Road Street</p>
+                    <p>Cumming, Georgia, 12345</p>
+                    <br />
+                    <p className="shiny-gold">(123)-456-7890</p>
+                </div>
+
+
+            </div>
+
+            {/* The bottom kapital bar */}
+            <div className="footer-bottom-bar">
+                <p className="shiny-gold">&copy; 2024 KAPITAL. All Rights Reserved.</p>
+            </div>
+
+        </footer>
+    )
+
 
 }
 
