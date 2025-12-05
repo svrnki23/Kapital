73ef51346faa294507f5488f9559ff8ded382bba
svrnki23 <srikar1330@gmail.com>
2025-12-04 22:32:59 -0600

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,37 +9,37 @@ const Footer = () => {
                 {/* Column 1 - brand and vision */}
                 <div className="footer-brand">
                     <h2 className="shiny-gold">KAPITAL</h2>
-                    <p>Experience the thrill of the drive. Premium luxury vehicles available at your command.</p>
+                    <p className="shiny-gold">Experience the thrill of the drive. Premium luxury vehicles available at your command.</p>
                 </div>
 
 
                 {/* Column 2 - explore links */}
                 <div className="footer-column">
                     <h4 className="shiny-gold">EXPLORE</h4>
-                    <a href="#fleet">THE GARAGE</a>
-                    <a href="#services">Services</a>
-                    <a href="#membership">Membership</a>
-                    <a href="#about">About</a>
+                    <a className="shiny-gold" href="#fleet">THE FLEET</a>
+                    <a className="shiny-gold" href="#services">SERVICES</a>
+                    <a className="shiny-gold" href="#membership">MEMBERSHIP</a>
+                    <a className="shiny-gold" href="#about">ABOUT</a>
                 </div>
 
 
                 {/* Column 3 - legal support  */}
                 <div className="footer-column">
                     <h4 className="shiny-gold">SUPPORT</h4>
-                    <a href="#help">Help Center</a>
-                    <a href="#terms">Terms & Conditions</a>
-                    <a href="#privacy">Privacy Policy</a>
-                    <a href="#contact">Contact</a>
+                    <a className="shiny-gold" href="#help">Help Center</a>
+                    <a className="shiny-gold" href="#terms">Terms & Conditions</a>
+                    <a className="shiny-gold" href="#privacy">Privacy Policy</a>
+                    <a className="shiny-gold" href="#contact">Contact</a>
                 </div>
 
-                {/* Column 4 - Contact info */}
+                {/* Column 4 - Contact info
                 <div className="footer-column">
                     <h4 className="shiny-gold">VISIT US</h4>
-                    <p>123 Road Street</p>
-                    <p>Cumming, Georgia, 12345</p>
-                    <br />
-                    <p className="shiny-gold">(123)-456-7890</p>
-                </div>
+                    <p className="shiny-gold">123 Road Street</p>
+                    <p className="shiny-gold">Cumming, Georgia, 12345</p>
+                    {/* <br /> */}
+                    {/* <p className="shiny-gold">(123)-456-7890</p>
+                </div> */}
 
 
             </div>
